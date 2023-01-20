febabfa018962731531472216c492fe0ee9cbff4
mkoerner-mgage <65774697+mkoerner-mgage@users.noreply.github.com>
2023-01-19 18:48:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 streamlit.title('My Parents New Healthy Diner')
 
